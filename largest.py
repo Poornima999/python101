2bcbebdfa7f2e0119d54e1055c4484b36cc2fcfb
Your Name <you@example.com>
2018-07-28 07:51:12 +0530

    Largest among 3 numbers

---
+++ largest.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+
+'''if (a>b) and (a >c):
+   largest = a
+elif (b >a) and (b > c):
+   largest = b
+else:
+   largest = c
+ 
+print("The largest number is",largest)'''
+
+
+
+if (a>b) and (a >c):
+    print("The largest number is",a)
+elif (b >a) and (b > c):
+    print("The largest number is",b)
+else:
+    print("The largest number is",c)
