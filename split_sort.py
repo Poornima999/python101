d2928b0be7af45687ead16caabc98d4fa1467878
Your Name <you@example.com>
2018-08-02 11:30:20 +0530

    use of split and sort functions

---
+++ split_sort.py
@@ -0,0 +1,5 @@
+sentence="Hello it is interesting to learn python"
+print(sentence)
+splitsentence=sentence.split()
+print("The splited sentence is:\n",splitsentence)
+print("The sorted sentence is:\n",sorted(splitsentence))
